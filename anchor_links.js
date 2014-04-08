091bf1b685e22bf64984a21f59bad8a52ea7475e
Chris Toomey <chris@ctoomey.com>
2014-04-08 09:33:26 -0400

    Hotkey trigger style application

--- anchor_links.js
+++ anchor_links.js
@@ -1,4 +1,12 @@
 $(function() {
-  anchored_elements = $('*[id]');
-  console.log('There are ' + anchored_elements.length + ' anchored elements')
+  var targetClass = 'anchor-link-target'
+  var anchoredElements = $('*[id]');
+
+  $(document).on('keydown keyup', function(event) {
+    if (event.metaKey && event.ctrlKey && event.shiftKey) {
+      anchoredElements.addClass(targetClass);
+    } else {
+      anchoredElements.removeClass(targetClass)
+    }
+  });
 })
