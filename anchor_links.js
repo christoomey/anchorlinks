ddadb0ce84321230f9b0e9ec222887bfb61c361a
Chris Toomey <chris@ctoomey.com>
2014-04-08 15:43:16 -0400

    Scope to only headings and p tags

--- anchor_links.js
+++ anchor_links.js
@@ -1,5 +1,7 @@
 $(function() {
   var targetClass = 'anchor-link-target'
+  var targetableTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+  var selector = targetableTags.map(function (tagname) { return tagname + '[id]'}).join(',')
   var anchoredElements = $('*[id]');
 
   $(document).on('keydown keyup', function(event) {
