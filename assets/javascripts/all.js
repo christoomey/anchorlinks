5d8e5cc7089b4867894e66ed0ef661029aeb6eaa
Chris Toomey <chris@ctoomey.com>
2014-04-08 19:43:14 -0400

    Use an image for the link

--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -4,11 +4,23 @@ $(function() {
   var selector = targetableTags.map(function (tagname) { return tagname + '[id]'}).join(',')
   var anchoredElements = $('*[id]');
 
+  var attachLink = function(target) {
+    var link = $('<a class="anchor-link"></a>');
+    target.prepend(link);
+    return link;
+  }
+
+  var linkEmAll = function() {
+    anchoredElements.each(function(i, element) {
+      link = attachLink($(element))
+    });
+  }
+
   $(document).on('keydown keyup', function(event) {
     if (event.metaKey && event.ctrlKey && event.shiftKey) {
-      anchoredElements.addClass(targetClass);
+      linkEmAll()
     } else {
-      anchoredElements.removeClass(targetClass)
+      $('a.anchor-link').remove()
     }
   });
 })
