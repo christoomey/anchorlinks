73a281d076e31f4eba22566d322f88f4e0b99bbe
Chris Toomey <chris@ctoomey.com>
2014-04-08 20:07:13 -0400

    Display links on hover

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ similar content that may be burried within a web page.
 
 Sample anchor links for reference:
 
-- http://bourbon.io/docs/#backface-visibility
-- http://underscorejs.org/#union
-- http://en.wikipedia.org/wiki/Taal_Lake#History
+- [http://bourbon.io/docs/#backface-visibility](http://bourbon.io/docs/#backface-visibility)
+- [http://underscorejs.org/#union](http://underscorejs.org/#union)
+- [http://en.wikipedia.org/wiki/Taal_Lake#History](http://en.wikipedia.org/wiki/Taal_Lake#History)
--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -1,26 +1,17 @@
 $(function() {
-  var targetClass = 'anchor-link-target'
-  var targetableTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
-  var selector = targetableTags.map(function (tagname) { return tagname + '[id]'}).join(',')
   var anchoredElements = $('*[id]');
 
-  var attachLink = function(target) {
-    var link = $('<a class="anchor-link"></a>');
+  var attachLink = function(event) {
+    var target = $(this)
+    var href = '#' + target.attr('id')
+    var link = $('<a class="anchor-link" href="'  + href + '"></a>');
     target.prepend(link);
     return link;
   }
 
-  var linkEmAll = function() {
-    anchoredElements.each(function(i, element) {
-      link = attachLink($(element))
-    });
+  var removeLink = function(event) {
+    $(this).find('a.anchor-link').remove()
   }
 
-  $(document).on('keydown keyup', function(event) {
-    if (event.metaKey && event.ctrlKey && event.shiftKey) {
-      linkEmAll()
-    } else {
-      $('a.anchor-link').remove()
-    }
-  });
+  anchoredElements.hover(attachLink, removeLink)
 })
